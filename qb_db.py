d08d16f959b952331ecb449b50ac84a8bcd0aadf
Quinn Downey <qdowney@cps.edu>
2025-10-06 11:32:36 -0500

    add kyler murray to qb_db

--- qb_db.py
+++ qb_db.py
@@ -2,3 +2,17 @@
 [(String player, String team, int passing yards, int rushing yards, float completion percentage, 
 int touchdowns, int interceptions thrown, int sacks)]
 """
+from typing import List, Tuple
+
+qb_db: List[Tuple[str, str, int, int, float, int, int, int]] = [
+    (
+        "kyler murray", #player name
+        "arizona cardinals", #player's team
+        3851, #passing yards
+        572, #rushing yards
+        68.8, #completion percentage
+        21, #touchdowns
+        11, #interceptions thrown
+        30, #sacks
+    ),
+]
