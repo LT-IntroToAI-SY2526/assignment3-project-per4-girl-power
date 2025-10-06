5d2954e6f3f3d14fb0701e1ff43eee9740cd6a1f
Quinn Downey <qdowney@cps.edu>
2025-10-06 11:19:17 -0500

    describe qb_db

--- qb_db.py
+++ qb_db.py
@@ -1,1 +1,4 @@
-
+"""This database stores the data for the 2024 season NFL quarterbacks in the following format
+[(String player, String team, int passing yards, int rushing yards, float completion percentage, 
+int touchdowns, int interceptions thrown, int sacks)]
+"""
