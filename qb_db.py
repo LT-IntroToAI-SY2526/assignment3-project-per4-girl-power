a5b296a4c192957bdba8963ddd23af506f9e10ac
VioletEZA <ezadan@cps.edu>
2025-10-07 10:52:28 -0500

    deleted

--- qb_db.py
+++
@@ -1,4 +0,0 @@
-"""This database stores the data for the 2024 season NFL quarterbacks in the following format
-[(String player, String team, int passing yards, int rushing yards, float completion percentage, 
-int touchdowns, int interceptions thrown, int sacks)]
-"""
